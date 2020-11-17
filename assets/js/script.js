59dfc0b04835ce65111007c01a30d371a4944a8e
Russ <ocjade@comcast.net>
2020-11-17 10:37:44 -0700

    Task Form Functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function () {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // console.log(taskTypeInput);
+    // console.dir(taskNameInput); View all information about the object.
+    // Create List Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new  task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    // Give Class Name
+    taskInfoEl.className = "task-info";
+
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
